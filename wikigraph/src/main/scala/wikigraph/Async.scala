949d096766d8fa95205b52087238ab7010c8d566
Lukasz Kielar <luk.kielar@gmail.com>
2024-02-17 08:25:54 +0000

    finish Async

--- wikigraph/src/main/scala/wikigraph/Async.scala
+++ wikigraph/src/main/scala/wikigraph/Async.scala
@@ -3,6 +3,8 @@ package wikigraph
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.Promise
+import scala.util.Failure
+import scala.util.Success
 import scala.util.Try
 import scala.util.control.NonFatal
 
@@ -16,7 +18,7 @@ object Async:
     * future with the same error.
     */
   def transformSuccess(eventuallyX: Future[Int]): Future[Boolean] =
-    ???
+    eventuallyX.map(_ % 2 == 0)
 
   /** Transforms a failed future value of type `Int` into a successful one
     * returning `-1`.
@@ -27,7 +29,9 @@ object Async:
     * successful future with the same value.
     */
   def recoverFailure(eventuallyX: Future[Int]): Future[Int] =
-    ???
+    eventuallyX.recoverWith { case e: Throwable =>
+      Future(-1)
+    }
 
   /** Performs two asynchronous computation, one after the other.
     * `asyncComputation2` should start ''after'' the `Future` returned by
@@ -43,7 +47,10 @@ object Async:
       asyncComputation1: () => Future[A],
       asyncComputation2: () => Future[B]
   ): Future[(A, B)] =
-    ???
+    for
+      r1 <- asyncComputation1()
+      r2 <- asyncComputation2()
+    yield (r1, r2)
 
   /** Concurrently performs two asynchronous computations and pair their
     * successful results together.
@@ -56,7 +63,13 @@ object Async:
       asyncComputation1: () => Future[A],
       asyncComputation2: () => Future[B]
   ): Future[(A, B)] =
-    ???
+    val f1 = asyncComputation1()
+    val f2 = asyncComputation2()
+
+    for
+      r1 <- f1
+      r2 <- f2
+    yield (r1, r2)
 
   /** Makes a chocolate cake.
     *
@@ -88,7 +101,11 @@ object Async:
       ) => Future[CakeDough],
       bake: CakeDough => Future[Cake]
   ): Future[Cake] =
-    ???
+    for
+      mix1 <- meltButterWithChocolate(butter, chocolate)
+      mix2 <- mixEverything(mix1, eggs, sugar)
+      mix3 <- bake(mix2)
+    yield mix3
 
   /** Attempts to perform an asynchronous computation at most `maxAttempts`
     * times.
@@ -102,6 +119,15 @@ object Async:
       asyncComputation: () => Future[A],
       maxAttempts: Int
   ): Future[A] =
-    ???
+    asyncComputation().transformWith {
+      case Failure(exception) => {
+        if maxAttempts <= 1 then {
+          throw exception
+        } else {
+          insist(asyncComputation, maxAttempts - 1)
+        }
+      }
+      case Success(value) => Future(value)
+    }
 
 end Async
